b7cc17da0c6d5d34dc889a9078fe431bfe889fe2
Michael Manto <michaelbmanto@gmail.com>
2018-07-01 15:47:53 +0100

    change RNG
    using old mersenne

--- AllStarSimulator.cpp
+++ AllStarSimulator.cpp
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <random>
 #include <time.h>
+#include "randomc.h"
+#include "mersenne.cpp"
 
 using namespace std;
 
-	std::mt19937_64 generator (int(time(0)));
+	//Set up the RNG
+	int seed = (int)time(0); 
+	CRandomMersenne RanGen(seed); 
 	
+
+	/*  WEIGHTED DICE 
+		weightedDice(int* dist)
+
+		Get a random number from a weighted distribution
+
+	 
+ 		dist: the weighted distribution - note: dist[0] must be the number of items
+		                                        dist[1] must be the total of the weightings
+
+ 		eg: weightedDice({3,100,20,60,20}) says, there are three items, that add up to 100. They are 20, 60 and 20
+
+		return: int - indicated the index between [ 0, dist[1] ]
+	*/
 	int weightedDice(int* dist)
 	{
 		int pick;
 
-		std::uniform_real_distribution<double> temp(0.0, dist[1]);
+		int dice = 	RanGen.IRandom(0, dist[1]-1);
 		
-		int dice = temp(generator);	
 		int checkpoint=0;
 
 		for (pick = 2; pick<=(dist[0]+1); pick++)
@@ -23,6 +40,7 @@ using namespace std;
 		return pick-2;
 	}
 
+	//Machine object
 	class EGM
 	{
 		public:
@@ -39,8 +57,10 @@ using namespace std;
 	}
 	
 
-	/*
+	/* PLAY GAME
+		Plays a game, ie it gets a game outcome and modifies the machine details
 		Assumes the player has the sufficient balance to play the game at the required bet
+
 		Returns -1 for all errors
 	*/
 	int playGame(EGM &machine, int bet)
@@ -112,8 +132,11 @@ int burndownrunner()
 
 int main()
 {
-	const int numTrials = 10000000;
+	 clock_t tStart = clock();
+	 
+	const int numTrials = 1000000;
 
+	
 	
 	
 	int diceRollRNGResult = 0;				// used to store the result from the RNG for the dice roll
@@ -121,6 +144,7 @@ int main()
 	int diceSelectRNGResult = -1; 			// used to store the result from the RNG for selecting the dice to use in a position
 	
 	double numGems = 0; 				// the number of gems collected in the trial
+	
 	double numIdols = 0;			// the number of ideols collected in the trial
 	double totalPrize=0;
 
@@ -141,28 +165,35 @@ int main()
 	const int RESPINRESULT = 1;
 	
 	
-	int diceWeights[2][8] = {
+	int diceWeights[3][8] = {
+						{6,6,2,3,1,0,0,0},
 						{6,6,1,1,1,1,1,1},
-						{6,6,1,1,1,1,1,1},
+						{6,6,1,1,1,1,1,1}
 						};
 	
-	int reRollWeights[2][4] = {
+	int reRollWeights[3][4] = {
 		                {2,10,1,9},
-		                {2,10,9,1}
+		                {2,10,9,1},
+						{2,10,5,5}
+						
 						};
 						
-	int gemWeights[2][7] = {
+	int gemWeights[3][7] = {
+							{5,7,1,2,2,1,1},
 							{5,5,1,1,1,1,1},
-							{5,5,1,1,1,1,1},
+							{5,7,1,1,2,2,1},
+							
 						};		
 	int gemValue[5] = {1,2,3,4,5};
 	
-	int whichDice[4] = {2,2,1,1};
+	int whichDice[5] = {3,3,1,1,1};
 	
-
+	int jackpotCollectionTiers[4] = {10,20,25,35};
+	int jackpotAmounts[4] = {20,150,1000,18000};
 	
 	for (int i=0; i<numTrials; i++) {
-		//numGems = 0;
+		if (i%(numTrials/100) ==0) printf("%f\n", (double)i/numTrials);
+		numGems = 0;
 		numIdols = 0;
 		
 		for (int j=0; j<15; j++)
@@ -186,7 +217,7 @@ int main()
 				{
 					numRerolls--;
 					diceRollRNGResult =  weightedDice(diceWeights[diceSelectRNGResult]);
-					//printf("...Roll %i", roll);	
+					//printf("...Roll");	
 					if (diceRollRNGResult == RESPINRESULT)
 					{					
 						rerollCheckRNGResult =  weightedDice(reRollWeights[diceSelectRNGResult]);
@@ -202,9 +233,19 @@ int main()
 			}
 		}
 		
+		
+		if (numGems >= jackpotCollectionTiers[3]) totalPrize+=jackpotAmounts[3];
+		else if (numGems >= jackpotCollectionTiers[2]) totalPrize+=jackpotAmounts[2];
+		else if (numGems >= jackpotCollectionTiers[1]) totalPrize+=jackpotAmounts[1];
+		else if (numGems >= jackpotCollectionTiers[0]) totalPrize+=jackpotAmounts[0];
+		
+		
 				
 	}
 	 printf("Prizes: %f\n", totalPrize/numTrials);
 	 printf("Gems %f\n", numGems/numTrials);
+	 
+	 	 time_t end = time(0); 
+		printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 		
 }
